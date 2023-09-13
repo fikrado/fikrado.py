dbd4e2815034f007a27d18db27d9ee1849f855f0
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2023-09-13 06:05:32 -1100

    Update termux.sh
    gf

--- termux.sh
+++ termux.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo
+tput bold
+tput setaf 40
+tput blink
+echo "$* **Fikrado.py is installing"
+tput sgr0
+tput sgr0
+tput sgr0
+echo
 echo "downloading python2 for hackers"
 apt install python2
 echo "downloading pip requrement"
