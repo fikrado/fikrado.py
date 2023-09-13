e3634d7aeab3ea1db7c376aa63a512fbdd497c62
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2023-09-13 06:05:46 -1100

    Update kali.sh
    i hate my ex

--- kali.sh
+++ kali.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo
+tput bold
+tput setaf 40
+tput blink
+echo "$* **Fikrado.py is installing"
+tput sgr0
+tput sgr0
+tput sgr0
+echo
 
 echo "setup kali for facebook hacking man "
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
